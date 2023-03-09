3a111e5e6f148c0eb04f012b2ccccf3ae062d50e
Nikita <gorbatenko.nik@bk.ru>
2023-03-09 23:55:49 +0700

    трехначное случайное и его вывод без второй цифры

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 1000-> {number}");
+
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+
+// int result = firstDigit * 10 + lastDigit;
+// Console.WriteLine($"Результат {result}");
+
+int removeSecondDigit = RemoveSecondDigit(number);
+Console.WriteLine($"Число без второй цифры {removeSecondDigit}");
+
+int RemoveSecondDigit (int num) // num = number
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
