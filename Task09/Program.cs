bce834ea043dc3535ddf7f77bbe254667b2eb270
Nikita <gorbatenko.nik@bk.ru>
2023-03-09 22:21:51 +0700

    случайное из отрезка от 10 до 99 и наибольшая цифра из случайного

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа ровна {firstDigit}");
+else
+Console.WriteLine($"Наибольшая цифра числа ровна {secondDigit}");
+
