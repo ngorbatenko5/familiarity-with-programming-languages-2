667b1c1f5c74dbef6f73aefe1c65533e8066cd57
Nikita <gorbatenko.nik@bk.ru>
2023-03-11 16:26:16 +0700

    кратно ли одновременно двум числам через метод

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -15,4 +15,16 @@ int number = Convert.ToInt32(Console.ReadLine());
 // {
 //     Console.WriteLine("нет");
 // };
-Bool Is
+bool IsDivided(int num)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+if (IsDivided(number))
+{
+Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
