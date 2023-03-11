f634f0591cd673807ade4265b36e467a2b9c617f
Nikita <gorbatenko.nik@bk.ru>
2023-03-11 17:03:10 +0700

    квадрат первого и второго числа

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsSquare(number1, number2) ? "ДА" : "Нет");
+
+bool IsSquare(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
