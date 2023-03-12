712a89c21fb3c90403bc2e76e7e70c0c48b2cfa9
Nikita <gorbatenko.nik@bk.ru>
2023-03-12 19:21:21 +0700

    при вводе цифры определяем выходный ли этот день

---
+++ Task15 дз/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите число дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Weekend(int Weekday)
+{
+    if (Weekday > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int result)
+{
+   if (result > 0 && result <= 7)
+   {
+    return true;
+   }
+   Console.WriteLine("Это не день недели");
+   return false;
+}
+
+if (ValidateWeekday(number))
+{
+    if (Weekend(number))
+    {
+    Console.WriteLine("Выходной");
+    }
+    else
+    {
+    Console.WriteLine("Работаем дальше");
+    }
+}
