4af12935411f6370c74ee66988a5a96f649816e2
Nikita <gorbatenko.nik@bk.ru>
2023-03-12 18:52:12 +0700

    Вывод третьей цифры заданного числа

---
+++ Task13 дз/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThirdDigit (int result)
+{
+    while (result > 999)
+    {
+    result /= 10;
+    }
+    return result % 10;
+}
+bool ValidateNumber(int result)
+{
+    if (result < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+if (ValidateNumber(number))
+{
+    Console.WriteLine(ThirdDigit(number));
+}
+
