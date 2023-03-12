4927d53977abbaae7f1ca6976d5897680a90c4e0
Nikita <gorbatenko.nik@bk.ru>
2023-03-12 18:17:11 +0700

    Вывод второй цифры трёзначного числа

---
+++ Task10 дз/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёзначное число");
+int result = Convert.ToInt32(Console.ReadLine());
+
+if (result < 100 || result >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+    return;
+}
+
+int secondDigit = result / 10 % 10;
+Console.WriteLine($"Вторая цифра введенного числа {secondDigit}");
+
+
